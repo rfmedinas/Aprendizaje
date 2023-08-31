23de2448846a1425cfc2c016503bdb1591e9dd71
Raúl Fernando Medina Sandoval <raulmedinasandoval@gmail.com>
2023-08-31 14:01:30 -0500

    cambio valor de grupo

--- src/main/java/com/inicio/aprendizaje/Ejemplo/AnimalRun.java
+++ src/main/java/com/inicio/aprendizaje/Ejemplo/AnimalRun.java
@@ -2,7 +2,7 @@ package com.inicio.aprendizaje.Ejemplo;
 
 public class AnimalRun {
     public static void  main(String[] args) {
-        Animal animal =new Animal("invertebrate","omnivore","acoustic");
-        LandAnimal landAnimal=new LandAnimal("vertebrate","carnivore","terrestrial",100);
+        Animal animal =new Animal("invertebrate","omnivore","salvage");
+        LandAnimal landAnimal=new LandAnimal("vertebrate","carnivore","Domestic",100);
     }
 }
